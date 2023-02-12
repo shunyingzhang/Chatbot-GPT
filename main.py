cde4d07c957e28fee3b833d954b73e529a233838
shunying <shunying.zhang@gmail.com>
2023-02-12 17:24:38 +1100

    Chatbot GPT GUI

---
+++ main.py
@@ -0,0 +1,44 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+import sys
+from backend import Chatbot
+import threading
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+        self.chatbot = Chatbot()
+
+        # Add chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        # Add input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
+
+        # Add submit button
+        self.button = QPushButton('Send', self)
+        self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
+
+        self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"Me: {user_input}")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color:#FFFFFF'>Chatbot: {response}</p>")
+
+
+app = QApplication(sys.argv)
+main_window = ChatbotWindow()
+sys.exit(app.exec())
